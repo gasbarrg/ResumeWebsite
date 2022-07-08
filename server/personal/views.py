934eb9da5355d390bd9d2bb03560145f39ca49ef
Gabe Gasbarre <78106315+gasbarrg@users.noreply.github.com>
2022-07-08 12:13:55 -0400

    Initial

---
+++ server/personal/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def index(request):
+    """View function for home page of site."""
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'home/index.html',)
+    
+def overview(request):
+    """View function for home page of site."""
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'home/overview.html',)
